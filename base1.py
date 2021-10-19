0d6731f3dc5236ac23110ae4ad712efce9b189e3
5at05h1 <69383363+5at05h1@users.noreply.github.com>
2021-10-19 14:57:32 +0900

    Python practice

---
+++ base1.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+print('Hello World')
+
+# コメント
+
+"""
+aaa
+いいい
+"""
+
+name = input('あなたの名前は何ですか？ : ')
+print(name)
+age = 31
+# format関数
+print('私の名前は = {}, 年齢は{}'.format(name, age))
+
+a = b = 'hello'
+
+print(a, b)
---
+++ base4.py
@@ -0,0 +1,23 @@
+# 数値型
+
+value = 1
+# print(value)
+# value = -2
+# print(value)
+# value = value + 4
+# print(value)
+# print(value * 4)
+# print(value / 3)
+# value = 10
+# print(value // 3)
+# print(value % 3)
+
+value += 3
+# print(value)
+# print(value ** 3)
+
+# 浮動小数点数
+height = 175.5
+
+print(height)
+print(type(height))
